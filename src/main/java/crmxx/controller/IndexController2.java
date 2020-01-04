3e262a53e95fc45a69410dd2f59ae8e75888e38d
hdonghui <email@example.com:623497386@qq.com>
2020-01-04 22:23:19 +0800

    第二次提交IndexController2修改

--- src/main/java/crmxx/controller/IndexController2.java
+++ src/main/java/crmxx/controller/IndexController2.java
@@ -3,15 +3,29 @@ package crmxx.controller;
 import base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("index2")
 public class IndexController2 extends BaseController {
 
+    @RequestMapping("index2")
     public String index2(){
         return "index2";
     }
+    @RequestMapping("list")
+    @ResponseBody
+    public List<String> getList(){
+        List<String> list=new ArrayList<>();
+        list.add("wosi");
+        list.add("nicai");
+        list.add("wbd");
+        return list;
+    }
+
 
 }
